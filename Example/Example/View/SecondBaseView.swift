e1d8ed2a4be0c5715866ecc70626fffe517845e7
shusuke0812 <shu.ota0812@gmail.com>
2021-05-02 20:03:42 +0900

    [add]SecondViewに検索バーを追加

--- Example/Example/View/SecondBaseView.swift
+++ Example/Example/View/SecondBaseView.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class SecondBaseView: UIView {
+    /// 検索バー
+    private let searchBar: UISearchBar = {
+        let searchBarTemp = UISearchBar()
+        searchBarTemp.placeholder = "記事を検索する"
+        searchBarTemp.translatesAutoresizingMaskIntoConstraints = false
+        return searchBarTemp
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        // UI初期設定
         self.configuredBasic()
+        
+        // UIをSuperVIewに追加
         self.addSubViews()
+        
+        // AutoLayout
+        self.setLayoutConstraint()
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -26,5 +39,17 @@ extension SecondBaseView {
     }
     // UIパーツの追加
     private func addSubViews() {
+        self.addSubview(self.searchBar)
+    }
+}
+// MARK: - Initialization SubView Method
+extension SecondBaseView {
+    private func setLayoutConstraint() {
+        NSLayoutConstraint.activate([
+            // 検索バー
+            self.searchBar.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            self.searchBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            self.searchBar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0)
+        ])
     }
 }
