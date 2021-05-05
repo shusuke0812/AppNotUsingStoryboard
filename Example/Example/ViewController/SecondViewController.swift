37d37d51fe5e2884331982ad048f41dd9f8509e3
shusuke0812 <shu.ota0812@gmail.com>
2021-05-05 16:59:39 +0900

    [add]SecondViewControllerにViewModelを接続しdelegate登録処理を追加、SearchBarのデリゲート処理も追加

--- Example/Example/ViewController/SecondViewController.swift
+++ Example/Example/ViewController/SecondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class SecondViewController: UIViewController {
     /// BaseView
     private var baseView = SecondBaseView()
+    /// ViewModel
+    private var viewModel = SecondViewModel()
 
     // MARK: - Lifecycle Method
     override func loadView() {
@@ -18,5 +20,15 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setDissmissKeyboard()
+        // デリゲート登録
+        self.baseView.searchBar.delegate = self
+        self.baseView.tableView.delegate = self
+        self.baseView.tableView.dataSource = self.viewModel
     }
+}
+// MARK: - SearchBar Delegate Method
+extension SecondViewController: UISearchBarDelegate {
+}
+// MARK: - TableView Delegate Method
+extension SecondViewController: UITableViewDelegate {
 }
