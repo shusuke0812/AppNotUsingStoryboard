847b6201135423c4d4d16876b2c1f265c396c61a
shusuke0812 <shu.ota0812@gmail.com>
2021-05-05 16:58:06 +0900

    [add]SecondViewModelにTableViewのDataSourceメソッドを追加

--- Example/Example/ViewModel/SecondViewModel.swift
+++ Example/Example/ViewModel/SecondViewModel.swift
@@ -5,4 +5,23 @@
 //  Created by Shusuke Ota on 2021/5/2.
 //
 
-import Foundation
+import UIKit
+
+class SecondViewModel: NSObject {
+    private(set) var sectionNumber = 1
+    private(set) var rowNumber = 30
+}
+// MARK: - UITableView DataSource Method
+extension SecondViewModel: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        self.sectionNumber
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.rowNumber
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Common.tableViewCellIdentifier, for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+}
