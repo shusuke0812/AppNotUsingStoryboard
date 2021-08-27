2aa5225c1b07cf5a5c248a760f3d0ca0af4e7238
shusuke0812 <shu.ota0812@gmail.com>
2021-08-27 23:54:47 +0900

    add: AppRootControllerを追加

--- Example/Example/App/AppDelegate.swift
+++ Example/Example/App/AppDelegate.swift
@@ -13,10 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = TopViewController()
-        self.window?.makeKeyAndVisible()
+        openTopPage()
         return true
     }
 
@@ -33,7 +30,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+// MARK: - Private
+extension AppDelegate {
+    private func openTopPage() {
+        let rootVC = AppRootController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootVC
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Example/Example/App/AppRootController.swift
@@ -0,0 +1,47 @@
+//
+//  AppRootController.swift
+//  Example
+//
+//  Created by Shusuke Ota on 2021/8/27.
+//
+
+import UIKit
+
+class AppRootController: UIViewController {
+
+    private var currentViewController: UIViewController?
+    
+    // Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showTopPage()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
+
+// MARK: - Private
+extension AppRootController {
+    private func showTopPage() {
+        let vc = UINavigationController(rootViewController: TopViewController())
+        changeViewController(vc)
+    }
+    private func changeViewController(_ vc: UIViewController) {
+        removeCurrentViewController()
+        setCurrentViewController(vc)
+    }
+    private func setCurrentViewController(_ vc: UIViewController) {
+        currentViewController = vc
+        addChild(vc)
+        view.addSubview(vc.view)
+        didMove(toParent: vc)
+    }
+    private func removeCurrentViewController() {
+        guard let vc = currentViewController else { return }
+        vc.willMove(toParent: nil)
+        vc.view.removeFromSuperview()
+        vc.removeFromParent()
+    }
+}
