4ec7ee46098af1100b64cee41059ac013a5544ef
shusuke0812 <shu.ota0812@gmail.com>
2021-08-29 18:21:37 +0900

    add: 検索画面作成

--- Example/Example/View/SearchBaseView.swift
+++ Example/Example/View/SearchBaseView.swift
@@ -8,13 +8,43 @@
 import UIKit
 
 class SearchBaseView: UIView {
+    
+    // 検索バー
+    private let searchBar: UISearchBar = {
+        let searchBarTemp = UISearchBar()
+        searchBarTemp.translatesAutoresizingMaskIntoConstraints = false
+        return searchBarTemp
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // UI初期設定
+        self.configBasic()
+        
+        // UIをSuperViewに追加
+        self.addSubViews()
+        
+        // AutoLayout
+        self.setLayoutConstraint()
     }
-    */
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
 
+// MARK: - Initialized
+extension SearchBaseView {
+    private func configBasic() {
+        self.backgroundColor = .white
+    }
+    private func addSubViews() {
+        self.addSubview(self.searchBar)
+    }
+    private func setLayoutConstraint() {
+        NSLayoutConstraint.activate([
+            self.searchBar.topAnchor.constraint(equalTo:  self.topAnchor, constant: 0),
+            self.searchBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            self.searchBar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0)
+        ])
+    }
 }
--- Example/Example/ViewController/SearchViewController.swift
+++ Example/Example/ViewController/SearchViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    /// BaseView
+    private var baseView = SearchBaseView()
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        self.view = self.baseView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
+}
 
+// MARK: - Initialized
+extension SearchViewController {
+    private func configBaseView() {}
 }
