8a329c6e39f70bd8fe354aa6a470fa1caeb958a4
shusuke0812 <shu.ota0812@gmail.com>
2021-08-29 15:40:57 +0900

    fix: SceneDelegate無効化、検索画面ファイル追加

---
+++ Example/Example/View/SearchBaseView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBaseView.swift
+//  Example
+//
+//  Created by Shusuke Ota on 2021/8/28.
+//
+
+import UIKit
+
+class SearchBaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
