b65d493a24455c71958e6de34024fbf06e3b6e45
shusuke0812 <shu.ota0812@gmail.com>
2021-11-27 08:11:18 +0900

    add: ハーフモーダル、領域外をタップして画面を閉じる処理を追加

--- Example/Example/ScreenModule/HalfModal/ViewController/FakeHalfModalViewController.swift
+++ Example/Example/ScreenModule/HalfModal/ViewController/FakeHalfModalViewController.swift
@@ -20,6 +20,13 @@ class FakeHalfModalViewController: UIViewController {
         
         setDelegate()
     }
+    // MARK: - Action
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        for touch in touches where touch.view == view {
+            presentingViewController?.dismiss(animated: true, completion: nil)
+        }
+    }
 }
 
 // MARK: -  Initialized
