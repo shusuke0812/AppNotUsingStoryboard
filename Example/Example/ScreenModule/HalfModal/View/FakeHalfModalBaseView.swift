6d24e1b8b02dd098f0512548f01014671b02f0ed
shusuke0812 <shu.ota0812@gmail.com>
2021-11-26 23:23:58 +0900

    add: ハーフモーダルで表示するContentViewを追加

--- Example/Example/ScreenModule/HalfModal/View/FakeHalfModalBaseView.swift
+++ Example/Example/ScreenModule/HalfModal/View/FakeHalfModalBaseView.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class FakeHalfModalBaseView: UIView {
+    /// ハーフモーダルで表示するContentView
+    private lazy var contentView: UIView = {
+        let tempView = UIView()
+        tempView.backgroundColor = .white
+        tempView.translatesAutoresizingMaskIntoConstraints = false
+        return tempView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        // UI初期設定
+        configBasic()
         
-        backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        // UIをSuperViewに追加
+        addSubViews()
+        
+        // Autolayout
+        setLayoutConstraint()
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+}
+
+// MARK: - Initialized
+extension FakeHalfModalBaseView {
+    /// 基本設定
+    private func configBasic() {
+        backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+    }
+    /// UIパーツの追加
+    private func addSubViews() {
+        addSubview(contentView)
+    }
+    /// Autolayout
+    private func setLayoutConstraint() {
+        NSLayoutConstraint.activate([
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 300)
+        ])
     }
 }
