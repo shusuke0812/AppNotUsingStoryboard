a8e20360bb75b4429f3a172c6c501d26fb9cf1e0
shusuke0812 <shu.ota0812@gmail.com>
2024-03-17 12:27:59 +0900

    add: button

--- SwiftUIExample/SwiftUIExample/ContentView.swift
+++ SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -17,6 +17,16 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundStyle(.blue)
+                .padding(.all, 10)
+            Button(action: {
+                debugPrint("DEBUG: Tap button")
+            }, label: {
+                Text("Tap")
+                    .font(.footnote)
+                    .fontWeight(.thin)
+                    .frame(width: 70, height: 30)
+                    .border(.blue, width: 0.5)
+            })
         }
         .padding()
     }
