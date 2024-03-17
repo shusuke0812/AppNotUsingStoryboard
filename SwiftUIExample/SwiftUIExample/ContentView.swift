32ce30e6c9b318997faadaf3550c4f729820039a
shusuke0812 <shu.ota0812@gmail.com>
2024-03-17 12:06:06 +0900

    add: SwiftUI project

---
+++ SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Shusuke Ota on 2024/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
