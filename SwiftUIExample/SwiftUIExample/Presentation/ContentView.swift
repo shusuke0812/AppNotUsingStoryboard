d856f089648321178a2882eb15d41e5102d89b3b
shusuke0812 <shu.ota0812@gmail.com>
2024-03-17 18:41:55 +0900

    fix: clipped Image

--- SwiftUIExample/SwiftUIExample/Presentation/ContentView.swift
+++ SwiftUIExample/SwiftUIExample/Presentation/ContentView.swift
@@ -46,6 +46,7 @@ struct ContentView: View {
                         .padding(),
                     alignment: .bottom
                 )
+                .clipShape(.capsule)
         }
         .padding()
     }
